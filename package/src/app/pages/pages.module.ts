12036bce8ddb7cc729556d751b9bb2c8a60abb42
wrappixelTeam <wrappixelteam.2016@gmail.com>
2023-10-17 14:28:28 +0530

    ''

---
+++ package/src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PagesRoutes } from './pages.routing.module';
+import { MaterialModule } from '../material.module';
+import { FormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
+// icons
+import { TablerIconsModule } from 'angular-tabler-icons';
+import * as TablerIcons from 'angular-tabler-icons/icons';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    NgApexchartsModule,
+    RouterModule.forChild(PagesRoutes),
+    TablerIconsModule.pick(TablerIcons),
+  ],
+  exports: [TablerIconsModule],
+})
+export class PagesModule {}
